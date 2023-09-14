363464609e466e22ee603c3a79a3f3bc6e4ebf4c
enzoborges <112429924+enzotmborges0204@users.noreply.github.com>
2023-09-14 11:25:31 -0300

    Update for.js

--- for.js
+++ for.js
@@ -1,5 +1,13 @@
+// Declaração de uma variável 'soma' inicializada com o valor 0
 let soma = 0;
+
+// Início de um loop 'for' com uma variável 'i' inicializada com 1;
+// O loop continuará enquanto 'i' for menor ou igual a 10;
+// Em cada iteração, 'i' é incrementado em 1.
 for (let i = 1; i <= 10; i++) {
+  // Adiciona o valor atual de 'i' à variável 'soma'.
   soma += i;
 }
+
+// Imprime a mensagem com o resultado da soma.
 console.log("A soma dos números de 1 a 10 é: " + soma);
