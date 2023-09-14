3be5f060937fd6058c949a2e998b54ec9de2dccc
enzoborges <112429924+enzotmborges0204@users.noreply.github.com>
2023-09-14 10:38:48 -0300

    Add files via upload

---
+++ switchcase.js
@@ -0,0 +1,14 @@
+const diaDaSemana = "quinta";
+switch (diaDaSemana) {
+  case "segunda":
+    console.log("Reunião de equipe às 10h.");
+    break;
+  case "quarta":
+    console.log("Treinamento de novos funcionários às 14h.");
+    break;
+  case "quinta":
+    console.log("Entrega de relatório semanal às 16h.");
+    break;
+  default:
+    console.log("Dia livre, sem compromissos.");
+}
